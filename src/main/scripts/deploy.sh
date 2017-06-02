eadad0e2c34aaafd11fadf03283fd0bb0d0e87fc
yermilov <yermilov17@gmail.com>
2017-06-03 00:22:08 +0300

    #2. catch up with recent devops practices

---
+++ src/main/scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+##############################################################################
+##
+##  Stop and kill currently running docker image, pull newest version and
+##  run it.
+##
+##############################################################################
+
+warn ( ) {
+    echo "$*"
+}
+
+warn "Currently running docker images"
+docker ps -a
+
+warn "Killing currently running docker image..."
+docker kill potic-aggregator; docker rm potic-aggregator
+
+warn "Pulling latest docker image..."
+docker pull potic/potic-aggregator:latest
+
+warn "Starting docker image..."
+docker run -dit --name potic-aggregator --link pocket-square-articles -v /logs:/logs -p 40401:8080 potic/potic-aggregator:latest
+
+warn "Currently running docker images"
+docker ps -a
