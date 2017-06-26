c59747309424373cf9b049b931625a78b93efdce
yermilov <yermilov17@gmail.com>
2017-06-27 01:56:57 +0300

    #3. fix issues with build

--- src/main/scripts/deploy.sh
+++ src/main/scripts/deploy.sh
@@ -18,10 +18,10 @@ warn "Killing currently running docker image..."
 docker kill potic-aggregator; docker rm potic-aggregator
 
 warn "Pulling latest docker image..."
-docker pull potic/potic-aggregator:latest
+docker pull potic/potic-aggregator:$TAG_TO_DEPLOY
 
 warn "Starting docker image..."
-docker run -dit --name potic-aggregator --link pocket-square-articles -v /logs:/logs -p 40401:8080 potic/potic-aggregator:latest
+docker run -dit --name potic-aggregator --link pocket-square-articles -v /logs:/logs -p 40401:8080 potic/potic-aggregator:$TAG_TO_DEPLOY
 
 warn "Currently running docker images"
 docker ps -a
