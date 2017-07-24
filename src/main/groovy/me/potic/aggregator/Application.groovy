6fb8490530f1c1108db6999f3838269c1dc82819
yermilov <yermilov17@gmail.com>
2017-07-25 01:33:10 +0300

    #9. try to set up security config

--- src/main/groovy/me/potic/aggregator/Application.groovy
+++ src/main/groovy/me/potic/aggregator/Application.groovy
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
-@PropertySource('classpath:auth0.properties')
 class Application {
 
     static void main(String[] args) {
--- src/main/groovy/me/potic/aggregator/config/SecurityConfig.groovy
+++ src/main/groovy/me/potic/aggregator/config/SecurityConfig.groovy
@@ -2,11 +2,15 @@ package me.potic.aggregator.config
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @EnableWebSecurity(debug = true)
 @Configuration
@@ -24,8 +28,21 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .forRS256(apiAudience, issuer)
                 .configure(http)
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, '/sandbox/**').permitAll()
-                .antMatchers(HttpMethod.OPTIONS, '/sandbox/**').permitAll()
+                .antMatchers(HttpMethod.GET, '/sandbox/**').fullyAuthenticated()
                 .anyRequest().authenticated()
+                .and().cors()
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration()
+        configuration.setAllowedOrigins( [ '*' ] )
+        configuration.setAllowedMethods( [ 'HEAD', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH' ] )
+        configuration.setAllowCredentials(true)
+        configuration.setAllowedHeaders( [ 'Authorization', 'Cache-Control', 'Content-Type' ])
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration('/sandbox/**', configuration)
+
+        source
     }
 }
