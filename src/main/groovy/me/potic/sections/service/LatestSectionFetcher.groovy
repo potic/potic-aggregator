c5c6341f0c68fafc99f497fd030bbfcd0e05b376
yermilov <yermilov17@gmail.com>
2017-12-15 23:48:44 +0200

    #22. change sections priority

--- src/main/groovy/me/potic/sections/service/LatestSectionFetcher.groovy
+++ src/main/groovy/me/potic/sections/service/LatestSectionFetcher.groovy
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service
 @Slf4j
 class LatestSectionFetcher implements SectionFetcher {
 
-    static final Section SECTION = new Section(id: 'latest', title: 'latest', priority: 2)
+    static final Section SECTION = new Section(id: 'latest', title: 'latest', priority: 1)
 
     @Autowired
     ArticlesService articlesService
